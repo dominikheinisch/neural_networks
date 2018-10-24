29ccbb6567ce608e266d1015ee8583ecc5d07aa3
dominik <dominikheinisch2@gmail.com>
2018-10-24 20:08:45 +0200

    changed Neuron to abstract

--- neural_networks/neuron/neurons/Neuron.py
+++ neural_networks/neuron/neurons/Neuron.py
@@ -1,25 +1,31 @@
+from abc import ABC, abstractmethod
 import numpy as np
 
 
-class Neuron:
+class Neuron(ABC):
     def __init__(self, **kwargs):
         # self.learn_param = kwargs['alpha']
         self.alpha = kwargs['alpha']
         self.invalid_output = kwargs['invalid_output']
-        self.learning_pairs = kwargs['learning_pairs']
+        self.learning_data = kwargs['learning_pairs']
         self.threshold = kwargs['threshold']
         self.weights = self.calc_weights(scope=kwargs['scope'])
         self.epochs = 0
 
-    def calc_weights(self, scope):
+    @staticmethod
+    def calc_weights(scope):
         return np.random.uniform(low=scope[0], high=scope[1], size=3)
 
     def learn(self):
-        input_pairs = list(self.learning_pairs.keys())
+        input_pairs = list(self.learning_data.keys())
         np.random.shuffle(input_pairs)
         while not all([self.is_output_correct(pair) for pair in input_pairs]):
             np.random.shuffle(input_pairs)
             self.epochs += 1
 
+    @abstractmethod
+    def is_output_correct(self, pair):
+        pass
+
     def __str__(self):
-        return '{0}\n{1}\n{2}\n'.format(self.epochs, self.weights, self.learning_pairs)
+        return '{0}\n{1}\n{2}\n'.format(self.epochs, self.weights, self.learning_data)
